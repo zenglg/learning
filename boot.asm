56f2094130be7f162d9eaaf24330ef26a869506a
Zeng Linggang <zenglg.jy@cn.fujitsu.com>
2014-11-23 23:10:57 +0800

    boot.asm: Add boot.asm
    # nasm boot.asm -o boot.bin
    # dd conv=sync if=boot.bin of=floppy bs=1440k count=1
    
    Signed-off-by: Zeng Linggang <zenglg.jy@cn.fujitsu.com>

---
+++ boot.asm
@@ -0,0 +1,24 @@
+;%define	_BOOT_DEBUG_
+%ifdef	_BOOT_DEBUG_
+	org 0100h
+%else
+	org 07c00h
+%endif
+	mov ax, cs
+	mov ds, ax
+	mov es, ax
+	call DispStr
+	jmp $
+DispStr:
+	mov ax, BootMessage
+	mov bp, ax
+	mov cx, 16
+	mov ax, 01301h
+	mov bx ,000ch
+	mov dl, 0
+	int 10h
+	ret
+BootMessage:       db "Hello, world!"
+times	510-($-$$) db 0
+
+dw	0xaa55
