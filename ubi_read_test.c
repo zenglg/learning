e3e90353bc147f2475ed3918432c2fd53fead2a2
Zeng Linggang <zenglg.jy@cn.fujitsu.com>
2015-09-22 11:25:46 +0800

    ubi_read_test.c: Some cleanup and fix and enhance
    Signed-off-by: Zeng Linggang <zenglg.jy@cn.fujitsu.com>

--- ubi_read_test.c
+++ ubi_read_test.c
@@ -19,16 +19,14 @@ MODULE_PARM_DESC(mode, "UBI device open mode");
 
 static int __init ubi_read_test_init(void)
 {
-	int i = 0;
 	int err;
 	int len;
 	int lnum = 0;
 	int offset = 0;
-	int check = 0;
 	char *buf = NULL;
+	char *write_buf = NULL;
 	struct ubi_volume_desc *desc;
-
-	pr_info("insmod ubi_read_test success.\n");
+	ktime_t begin, after, time_consuming;
 
 	desc = ubi_open_volume(ubi_num, vol_id, mode);
 	if (IS_ERR(desc)) {
@@ -41,7 +39,6 @@ static int __init ubi_read_test_init(void)
 	pr_info("usable_leb_size: %d\n", desc->vol->usable_leb_size);
 	pr_info("used_ebs: %d\n", desc->vol->used_ebs);
 	pr_info("used_bytes: %lld\n", desc->vol->used_bytes);
-	pr_info("ch_lnum: %d\n", desc->vol->ch_lnum);
 
 	len = desc->vol->usable_leb_size;
 	err = -ENOMEM;
@@ -49,18 +46,42 @@ static int __init ubi_read_test_init(void)
 	if (!buf)
 		goto out;
 
-	for (i = 0; i < desc->vol->used_ebs; i++) {
-		err = ubi_leb_read(desc, lnum, buf, offset, len, check);
+	write_buf = kmalloc(len, GFP_KERNEL);
+	if (!write_buf)
+		goto out;
+
+	memset(write_buf, 'a', len);
+
+	for (lnum = 0; lnum < desc->vol->used_ebs; lnum++) {
+		err = ubi_leb_write(desc, lnum, write_buf, offset, len);
 		if (err < 0) {
-			pr_info("ubi_leb_read error\n");
+			pr_info("ubi_leb_write error\n");
 			goto out;
 		}
 	}
-	pr_info("ch_lnum: %d\n", desc->vol->ch_lnum);
+
+	begin = ktime_get();
+
+	for (lnum = 0; lnum < desc->vol->used_ebs; lnum++) {
+		err = ubi_read(desc, lnum, buf, offset, len);
+		if (err < 0) {
+			pr_info("ubi_read error\n");
+			goto out;
+		}
+	}
+
+	after = ktime_get();
+
+	pr_info("insmod ubi_read_test success.\n");
+
+	time_consuming = ktime_sub(after, begin);
+
+	pr_info("Time consuming: %llums -- %lluus\n",
+		time_consuming.tv64 / 1000000, time_consuming.tv64 / 1000);
 
 out:
-	if (buf)
-		kfree(buf);
+	kfree(write_buf);
+	kfree(buf);
 	if (!IS_ERR(desc))
 		ubi_close_volume(desc);
 	return err;
