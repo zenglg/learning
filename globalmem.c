54cbc45d408506c5f38d3fcf64b092219cfdab22
Zeng Linggang <zenglg.jy@cn.fujitsu.com>
2015-12-26 23:38:42 +0800

    globalmem.c: fix some errors to make it work
    Signed-off-by: Zeng Linggang <zenglg.jy@cn.fujitsu.com>

--- globalmem.c
+++ globalmem.c
@@ -6,13 +6,11 @@
 #include <linux/sched.h>
 #include <linux/init.h>
 #include <linux/cdev.h>
-#include <asm/io.h>
-#include <asm/system.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
 #define GLOBALMEM_SIZE	0x1000
 #define MEM_CLEAR	0x1
-#define GLOBALMEM_MAJOR	250
+#define GLOBALMEM_MAJOR	230
 
 static int globalmem_major = GLOBALMEM_MAJOR;
 
@@ -23,51 +21,8 @@ struct globalmem_dev {
 
 struct globalmem_dev dev;
 
-int globalmem_init(void)
-{
-	int result;
-	dev_t devno = MKDEV(globalmem_major, 0);
-
-	if (globalmeme_major) {
-		result = register_chrdev_region(devno, 1, "globalmem");
-	} else {
-		result = alloc_chrdev_region(&devno, 0, 1, "globalmem");
-		globalmem_major = MAJOR(devno);
-	}
-
-	if (result < 0)
-		return result;
-
-	globalmem_setup_cdev();
-	return 0;
-}
-
-void globalmem_exit(void)
-{
-	cdev_del(&dev.cdev);
-	unregister_chrdev_region(MKDEV(globalmem_major, 0), 1);
-}
-
-static void globalmem_setup_cdev()
-{
-	int err, devno = MKDEV(globalmem_major, 0);
-
-	cdev_init(&dev.cdev, &globalmem_fops);
-	dev.cdev.owner = THIS_MODULE;
-	err = cdev_add(&dev.cdev, devno, 1);
-	if (err)
-		printk(KERN_NOTICE "Error %d adding globalmem", err);
-}
-
-static const struct file_operations globalmem_fops = {
-	.owner = THIS_MODULE,
-	.llseek = globalmem_llseek,
-	.read = globalmem_read,
-	.write = globalmem_write,
-	.ioctl = globalmem_ioctl,
-};
-
-static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t count, loff_t *ppos)
+static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t count,
+			      loff_t *ppos)
 {
 	unsigned long p = *ppos;
 	int ret = 0;
@@ -82,13 +37,14 @@ static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t count,
 	} else {
 		*ppos += count;
 		ret = count;
-		printk(KERN_INFO "read %d bytes(s) from %d\n", count, p);
+		printk(KERN_INFO "read %ld bytes(s) from %lu\n", count, p);
 	}
 
 	return ret;
 }
 
-static ssize_t globalmem_write(struct file *filp, const char __user *buf, size_t count, loff_t *ppos)
+static ssize_t globalmem_write(struct file *filp, const char __user *buf,
+			       size_t count, loff_t *ppos)
 {
 	unsigned long p = *ppos;
 	int ret = 0;
@@ -103,13 +59,13 @@ static ssize_t globalmem_write(struct file *filp, const char __user *buf, size_t
 	} else {
 		*ppos += count;
 		ret = count;
-		printk(KERN_INFO "written %d bytes(s) from %d\n", count, p);
+		printk(KERN_INFO "written %ld bytes(s) from %lu\n", count, p);
 	}
 
 	return ret;
 }
 
-static ssize_t globalmem_llseek(struct file *filp, loff_t offset, int orig) 
+static loff_t globalmem_llseek(struct file *filp, loff_t offset, int orig) 
 {
 	loff_t ret;
 
@@ -145,11 +101,12 @@ static ssize_t globalmem_llseek(struct file *filp, loff_t offset, int orig)
 	return ret;
 }
 
-static int globalmem_ioctl(struct inode *inodep, struct file *filp, unsigned int cmd, unsigned long arg)
+static long globalmem_ioctl(struct file *filp, unsigned int cmd,
+			    unsigned long arg)
 {
 	switch (cmd) {
 	case MEM_CLEAR:
-		memset(dev->mem, 0, GLOBALMEM_SIZE);
+		memset(dev.mem, 0, GLOBALMEM_SIZE);
 		printk(KERN_INFO "globalmem is set to zero\n");
 		break;
 	default:
@@ -157,3 +114,52 @@ static int globalmem_ioctl(struct inode *inodep, struct file *filp, unsigned int
 	}
 	return 0;
 }
+
+static const struct file_operations globalmem_fops = {
+	.owner = THIS_MODULE,
+	.llseek = globalmem_llseek,
+	.read = globalmem_read,
+	.write = globalmem_write,
+	.unlocked_ioctl = globalmem_ioctl,
+};
+
+static void globalmem_setup_cdev(void)
+{
+	int err, devno = MKDEV(globalmem_major, 0);
+
+	cdev_init(&dev.cdev, &globalmem_fops);
+	dev.cdev.owner = THIS_MODULE;
+	err = cdev_add(&dev.cdev, devno, 1);
+	if (err)
+		printk(KERN_NOTICE "Error %d adding globalmem", err);
+}
+
+static int globalmem_init(void)
+{
+	int result;
+	dev_t devno = MKDEV(globalmem_major, 0);
+
+	if (globalmem_major) {
+		result = register_chrdev_region(devno, 1, "globalmem");
+	} else {
+		result = alloc_chrdev_region(&devno, 0, 1, "globalmem");
+		globalmem_major = MAJOR(devno);
+	}
+
+	if (result < 0)
+		return result;
+
+	globalmem_setup_cdev();
+	return 0;
+}
+
+static void globalmem_exit(void)
+{
+	cdev_del(&dev.cdev);
+	unregister_chrdev_region(MKDEV(globalmem_major, 0), 1);
+}
+
+module_init(globalmem_init);
+module_exit(globalmem_exit);
+MODULE_AUTHOR("Zeng");
+MODULE_LICENSE("GPL v2");
