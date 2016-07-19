c4809c50a0c522ec60e9bbf34f187e9c93106cb8
Zeng Linggang <zenglg.jy@cn.fujitsu.com>
2016-07-19 17:28:12 +0800

    s/workqueu/work_queue
    Signed-off-by: Zeng Linggang <zenglg.jy@cn.fujitsu.com>

--- workqueue.c
+++ workqueue.c
@@ -9,19 +9,19 @@
 static struct workqueue_struct *queue = NULL;
 static struct work_struct *work = NULL;
 
-static void workqueue_handler(struct work_struct *w)
+static void work_queue_handler(struct work_struct *w)
 {
 	printk(KERN_ALERT "work handler function.\n");
 	udelay(100);
 }
 
-static int __init workqueue_init(void)
+static int __init work_queue_init(void)
 {
 	int ret = 0;
 	int i;
 
 	/* 创建一个单线程的工作队列 */
-	queue = create_singlethread_workqueue("workqueue");
+	queue = create_singlethread_workqueue("work_queue");
 	if (!queue) {
 		ret = -1;
 		goto err;
@@ -34,7 +34,7 @@ static int __init workqueue_init(void)
 	}
 
 	for (i = 0; i < NUM_WORK; i++) {
-		INIT_WORK(&work[i], workqueue_handler);
+		INIT_WORK(&work[i], work_queue_handler);
 		queue_work(queue, &work[i]);
 	}
 
@@ -43,7 +43,7 @@ err:
 	return ret;
 }
 
-static void __exit workqueue_exit(void)
+static void __exit work_queue_exit(void)
 {
 	if (work)
 		kfree(work);
@@ -51,7 +51,7 @@ static void __exit workqueue_exit(void)
 	destroy_workqueue(queue);
 }
 
-module_init(workqueue_init);
-module_exit(workqueue_exit);
+module_init(work_queue_init);
+module_exit(work_queue_exit);
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("Zeng Linggang");
