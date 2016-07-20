6a6097fe8965db698dea4693641e4430a746f9e3
Zeng Linggang <zenglg.jy@cn.fujitsu.com>
2016-07-20 09:43:22 +0800

    work_queue: Use struct
    Signed-off-by: Zeng Linggang <zenglg.jy@cn.fujitsu.com>

--- work_queue.c
+++ work_queue.c
@@ -6,8 +6,10 @@
 
 #define NUM_WORK	10
 
-static struct workqueue_struct *queue = NULL;
-static struct work_struct *work = NULL;
+static struct test {
+	struct workqueue_struct	*queue;
+	struct work_struct	*work;
+} *t;
 
 static void work_queue_handler(struct work_struct *w)
 {
@@ -20,35 +22,51 @@ static int __init work_queue_init(void)
 	int ret = 0;
 	int i;
 
-	/* 创建一个单线程的工作队列 */
-	queue = create_singlethread_workqueue("work_queue");
-	if (!queue) {
-		ret = -1;
-		goto err;
+	t = kmalloc(sizeof(struct test), GFP_KERNEL);
+	if (!t) {
+		ret = -ENOMEM;
+		goto out;
 	}
 
-	work = kmalloc(sizeof(struct work_struct) * NUM_WORK, GFP_KERNEL);
-	if (!work) {
+	t->queue = create_singlethread_workqueue("work_queue");
+	if (!t->queue) {
 		ret = -ENOMEM;
-		goto err;
+		goto free_t;
+	}
+
+	t->work = kmalloc(sizeof(struct work_struct) * NUM_WORK, GFP_KERNEL);
+	if (!t->work) {
+		ret = -ENOMEM;
+		goto destroy_queue;
 	}
 
 	for (i = 0; i < NUM_WORK; i++) {
-		INIT_WORK(&work[i], work_queue_handler);
-		queue_work(queue, &work[i]);
+		INIT_WORK(&t->work[i], work_queue_handler);
+		queue_work(t->queue, &t->work[i]);
 	}
 
-	return 0;
-err:
+	return ret;
+
+destroy_queue:
+	destroy_workqueue(t->queue);
+
+free_t:
+	if (t)
+		kfree(t);
+
+out:
 	return ret;
 }
 
 static void __exit work_queue_exit(void)
 {
-	if (work)
-		kfree(work);
+	if (t->work)
+		kfree(t->work);
 
-	destroy_workqueue(queue);
+	destroy_workqueue(t->queue);
+
+	if (t)
+		kfree(t);
 }
 
 module_init(work_queue_init);
