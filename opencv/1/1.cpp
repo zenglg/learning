4666aef4a4ef236e95c8a37a9d76074350fe5387
Linggang Zeng <linggang.zeng@easystack.cn>
2019-01-06 19:51:54 +0000

    Add opencv tests
    Signed-off-by: Linggang Zeng <linggang.zeng@easystack.cn>

---
+++ opencv/1/1.cpp
@@ -0,0 +1,13 @@
+#include <opencv/highgui.h>
+
+int main(int argc, char *argv[])
+{
+	IplImage *img = cvLoadImage(argv[1]);
+	cvNamedWindow("Example1", CV_WINDOW_AUTOSIZE);
+	cvShowImage("Example1", img);
+	cvWaitKey(0);
+	cvReleaseImage(&img);
+	cvDestroyWindow("Example1");
+
+	return 0;
+}
